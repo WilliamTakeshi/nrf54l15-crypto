32ad39aa508c438c724b2f6a952809ad43f1ee54
WilliamTakeshi <williamtpereira@gmail.com>
2025-10-03 16:24:06 +0200

    chore: add blink example

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# stm32wba55-crypto
+
+Minimal Rust + nRF54L15 example project.
+
+## Setup
+
+1. **Install JLink software and documentation**  
+   ```bash
+   yay -S jlink-software-and-documentation
+   ```
+2. **Install nrf-udev**
+   ```bash
+   yay -S nrf-udev
+   ```
+3. **Connect the nRF54L15 and run the example**
+    ```bash
+    cargo run --bin blink
+    ```
---
+++ src/bin/blinky.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+
+    // https://docs.nordicsemi.com/bundle/ug_nrf54l15_dk/page/UG/nRF54L15_DK/hw_desription/buttons_leds.html
+    let mut led1 = Output::new(p.P2_09, Level::Low, OutputDrive::Standard);
+    let mut led2 = Output::new(p.P1_10, Level::Low, OutputDrive::Standard);
+    let mut led3 = Output::new(p.P2_07, Level::Low, OutputDrive::Standard);
+    let mut led4 = Output::new(p.P1_14, Level::Low, OutputDrive::Standard);
+
+    loop {
+        led1.set_high();
+        led2.set_high();
+        led3.set_high();
+        led4.set_high();
+        Timer::after_millis(1000).await;
+        led1.set_low();
+        led2.set_low();
+        led3.set_low();
+        led4.set_low();
+        Timer::after_millis(1000).await;
+    }
+}
