81688d2eeb31c7a08c6b94986556399e7728c605
William Takeshi Pereira <williamtpereira@gmail.com>
2025-10-15 09:29:47 -0300

    add a Blinky pac example (#1)

---
+++ src/bin/blinky-pac.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use defmt::info;
+use defmt_rtt as _;
+use nrf54l15_app_pac as pac;
+use panic_probe as _;
+
+#[entry]
+fn main() -> ! {
+    info!("Starting nRF54L15 blinky example...");
+    let p = pac::Peripherals::take().unwrap();
+    p.global_p2_s.pin_cnf(9).write(|w| w.dir().output());
+    p.global_p1_s.pin_cnf(10).write(|w| w.dir().output());
+    p.global_p2_s.pin_cnf(7).write(|w| w.dir().output());
+    p.global_p1_s.pin_cnf(14).write(|w| w.dir().output());
+
+    loop {
+        // Turn LED on
+        p.global_p2_s.outset().write(|w| w.pin9().bit(true));
+        p.global_p1_s.outset().write(|w| w.pin10().bit(true));
+        p.global_p2_s.outset().write(|w| w.pin7().bit(true));
+        p.global_p1_s.outset().write(|w| w.pin14().bit(true));
+
+        for _ in 0..100_000 {
+            cortex_m::asm::nop();
+        }
+
+        // Turn LED off
+        p.global_p2_s.outclr().write(|w| w.pin9().bit(true));
+        p.global_p1_s.outclr().write(|w| w.pin10().bit(true));
+        p.global_p2_s.outclr().write(|w| w.pin7().bit(true));
+        p.global_p1_s.outclr().write(|w| w.pin14().bit(true));
+
+        for _ in 0..100_000 {
+            cortex_m::asm::nop();
+        }
+    }
+}
