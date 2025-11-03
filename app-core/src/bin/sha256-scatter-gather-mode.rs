da33d227593eead376f8853ed2688161da3c5254
WilliamTakeshi <williamtpereira@gmail.com>
2025-11-03 15:09:08 +0100

    chore: tidy up code

--- app-core/src/bin/sha256-scatter-gather-mode.rs
+++ app-core/src/bin/sha256-scatter-gather-mode.rs
@@ -2,18 +2,34 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use defmt::{info, println};
+use defmt::info;
 use defmt_rtt as _;
 use nrf54l15_app_pac;
 use panic_probe as _;
 
+// Supported hash algorithm bitmasks
+#[allow(dead_code)]
+#[repr(u8)]
+#[derive(Copy, Clone, Debug)]
+enum HashAlg {
+    // Sha1 = 0x02, // TODO: implement
+    Sha2_224 = 0x04,
+    Sha2_256 = 0x08,
+    Sha2_384 = 0x10,
+    Sha2_512 = 0x20,
+    // Sm3 = 0x40,      // TODO: implement
+}
+
 #[entry]
 fn main() -> ! {
     info!("Starting nRF54L15 CryptoMaster SHA-256 example...");
+    const OUTPUT_BUF_LEN: usize = 28;
+    const HASH_ALGO: u8 = HashAlg::Sha2_224 as u8;
+    static mut INPUT: [u8; 14] = *b"Example string";
 
     let p = nrf54l15_app_pac::Peripherals::take().unwrap();
 
-    let mut output_buf: [u8; 32] = [0x00; 32];
+    let mut output_buf: [u8; OUTPUT_BUF_LEN] = [0x00; OUTPUT_BUF_LEN];
     let output_buf_ptr = output_buf.as_mut_ptr();
 
     // Last descriptor
@@ -21,24 +37,30 @@ fn main() -> ! {
 
     // Outer descriptor (top-level m)
     let mut output_outer = SxDesc {
-        addr: output_buf_ptr, // <(OUTPUT)>
+        addr: output_buf_ptr,
         next: last_descriptor,
-        sz: 536870944, // 0x20000010
+        sz: (0x20000000 + OUTPUT_BUF_LEN as u32),
         dmatag: 32,
     };
 
     // Create the actual string in memory
 
-    static mut EXAMPLE_STR: [u8; 18] = *b"Example string1234";
-    let mut bytes_0806: [u8; 4] = [0x08, 0x06, 0x00, 0x00];
-    let addr_0806 = bytes_0806.as_mut_ptr();
+    let mut bytes_hash: [u8; 4] = [HASH_ALGO, 0x06, 0x00, 0x00];
+    let addr_hash = bytes_hash.as_mut_ptr();
 
     fn dmatag_for(input: usize) -> u32 {
+        const TAG_BASE: u32 = 0x23;
+        const TAG_0: u32 = 0x000;
+        const TAG_1: u32 = 0x300;
+        const TAG_2: u32 = 0x200;
+        const TAG_3: u32 = 0x100;
+
         match input % 4 {
-            0 => 35,
-            1 => 803,
-            2 => 547,
-            _ => 291,
+            0 => TAG_BASE | TAG_0, // -> 0x023 = 35
+            1 => TAG_BASE | TAG_1, // -> 0x323 = 803
+            2 => TAG_BASE | TAG_2, // -> 0x223 = 547
+            3 => TAG_BASE | TAG_3, // -> 0x123 = 291
+            _ => panic!("impossible state"),
         }
     }
 
@@ -48,15 +70,13 @@ fn main() -> ! {
     }
 
     // TODO: remove this unsafe by making EXAMPLE_STR not mut
-    let len = unsafe { (&(*(&raw const EXAMPLE_STR))).len() };
+    let len = unsafe { (&(*(&raw const INPUT))).len() };
     let dmatag = dmatag_for(len);
     let sz = sz(len);
 
-    println!("dmatag: {}", dmatag);
-
     // Middle descriptor
     let mut input_mid = SxDesc {
-        addr: core::ptr::addr_of_mut!(EXAMPLE_STR) as *mut u8, // "Example string"
+        addr: core::ptr::addr_of_mut!(INPUT) as *mut u8, // "Example string"
         next: last_descriptor,
         sz,
         dmatag,
@@ -64,7 +84,7 @@ fn main() -> ! {
 
     // Outer descriptor (m)
     let mut input_outer = SxDesc {
-        addr: addr_0806,
+        addr: addr_hash,
         next: &mut input_mid as *mut SxDesc,
         sz: 536870916, // 0x20000004
         dmatag: 19,
@@ -109,7 +129,7 @@ fn main() -> ! {
 
     // output
     unsafe {
-        let bytes = core::slice::from_raw_parts(output_buf_ptr, 32);
+        let bytes = core::slice::from_raw_parts(output_buf_ptr, output_buf.len());
         info!("output bytes: {:02x}", bytes);
     }
 
