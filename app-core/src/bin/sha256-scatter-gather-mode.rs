0a50b39236d1d57d57bd42145a5ba02c40c0f1bf
William Takeshi Pereira <williamtpereira@gmail.com>
2025-11-03 11:26:48 -0300

    Improve dma tags (#4)

--- app-core/src/bin/sha256-scatter-gather-mode.rs
+++ app-core/src/bin/sha256-scatter-gather-mode.rs
@@ -4,49 +4,76 @@
 use cortex_m_rt::entry;
 use defmt::info;
 use defmt_rtt as _;
-use nrf54l15_app_pac;
 use panic_probe as _;
+
+// Supported hash algorithm bitmasks
+#[allow(dead_code)]
+#[repr(u8)]
+#[derive(Copy, Clone, Debug)]
+enum HashAlg {
+    Sha1 = 0x02,
+    Sha2_224 = 0x04,
+    Sha2_256 = 0x08,
+    Sha2_384 = 0x10,
+    Sha2_512 = 0x20,
+    // Sm3 = 0x40,      // TODO: implement
+}
+
+const fn hash_output_len(algo: HashAlg) -> usize {
+    match algo {
+        HashAlg::Sha1 => 20,
+        HashAlg::Sha2_224 => 28,
+        HashAlg::Sha2_256 => 32,
+        HashAlg::Sha2_384 => 48,
+        HashAlg::Sha2_512 => 64,
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    info!("Starting nRF54L15 CryptoMaster SHA-256 example...");
+    info!("Starting nRF54L15 CryptoMaster SHA example...");
+    const HASH_ALGO: HashAlg = HashAlg::Sha1;
+    const OUTPUT_BUF_LEN: usize = hash_output_len(HASH_ALGO);
+    const INPUT_BUF_LEN: usize = 14;
+    static mut INPUT: [u8; INPUT_BUF_LEN] = *b"Example string";
 
     let p = nrf54l15_app_pac::Peripherals::take().unwrap();
 
-    let mut output_buf: [u8; 32] = [0x00; 32];
+    let mut output_buf: [u8; OUTPUT_BUF_LEN] = [0x00; OUTPUT_BUF_LEN];
     let output_buf_ptr = output_buf.as_mut_ptr();
 
+    let mut bytes_hash: [u8; 4] = [HASH_ALGO as u8, 0x06, 0x00, 0x00];
+    let addr_hash = bytes_hash.as_mut_ptr();
+
+    let dmatag = dmatag_for(INPUT_BUF_LEN);
+    let sz = sz(INPUT_BUF_LEN);
+
     // Last descriptor
+    #[allow(
+        clippy::manual_dangling_ptr,
+        reason = "nRF54L15 needs this pointer to be on address 1"
+    )]
     let last_descriptor: *mut SxDesc = 1 as *mut SxDesc;
 
-    // Outer descriptor (top-level m)
+    // Outer descriptor (output)
     let mut output_outer = SxDesc {
-        addr: output_buf_ptr, // <(OUTPUT)>
+        addr: output_buf_ptr,
         next: last_descriptor,
-        sz: 536870944, // 0x20000010
+        sz: (0x20000000 + OUTPUT_BUF_LEN as u32),
         dmatag: 32,
     };
 
-    // Create the actual string in memory
-    static mut EXAMPLE_STR: [u8; 17] = *b"Example string123";
-    let mut bytes_0806: [u8; 4] = [0x08, 0x06, 0x00, 0x00];
-    let addr_0806 = bytes_0806.as_mut_ptr();
-
-    // Middle descriptor
+    // Middle descriptor (input)
     let mut input_mid = SxDesc {
-        addr: core::ptr::addr_of_mut!(EXAMPLE_STR) as *mut u8, // "Example string"
+        addr: core::ptr::addr_of_mut!(INPUT) as *mut u8, // "Example string"
         next: last_descriptor,
-        // sz: 536870928, // 0x20000010 == 16
-        sz: 536870932, // 0x20000014 == 20 (after EXAMPLE_STR sz 17)
-        // dmatag: 547, // *b"Example string\0";
-        // dmatag: 291, //*b"Example string1\0";
-        // dmatag: 35, //*b"Example string12";
-        dmatag: 803, //*b"Example string123";
+        sz,
+        dmatag,
     };
 
-    // Outer descriptor (m)
+    // Outer descriptor (input)
     let mut input_outer = SxDesc {
-        addr: addr_0806,
+        addr: addr_hash,
         next: &mut input_mid as *mut SxDesc,
         sz: 536870916, // 0x20000004
         dmatag: 19,
@@ -90,14 +117,32 @@ fn main() -> ! {
     while dma.status().read().pushbusy().bit_is_set() {}
 
     // output
-    unsafe {
-        let bytes = core::slice::from_raw_parts(output_buf_ptr, 32);
-        info!("output bytes: {:02x}", bytes);
-    }
+    info!("output bytes: {:02x}", output_buf);
 
     loop {
         cortex_m::asm::nop();
     }
+}
+
+fn dmatag_for(input: usize) -> u32 {
+    const TAG_BASE: u32 = 0x23;
+    const TAG_0: u32 = 0x000;
+    const TAG_1: u32 = 0x300;
+    const TAG_2: u32 = 0x200;
+    const TAG_3: u32 = 0x100;
+
+    match input % 4 {
+        0 => TAG_BASE | TAG_0, // -> 0x023 = 35
+        1 => TAG_BASE | TAG_1, // -> 0x323 = 803
+        2 => TAG_BASE | TAG_2, // -> 0x223 = 547
+        3 => TAG_BASE | TAG_3, // -> 0x123 = 291
+        _ => panic!("impossible state"),
+    }
+}
+
+fn sz(n: usize) -> u32 {
+    let group_end = ((n - 1) / 4 + 1) * 4;
+    (group_end | 0x2000_0000).try_into().unwrap()
 }
 
 #[repr(C)]
